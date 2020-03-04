60d5f3152f1b64f7e96bbd10b64f52f5cffdcda2
BogdanPesov <bodyapesov@gmail.com>
2020-03-04 19:01:58 +0400

    slider done

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,15 @@
 $(function() {
+    $(".slider__item-star").rateYo({
+        rating: 4.5,
+        starWidth: "14px",
+        readOnly: true
+    });
+
+    $('.slider__inner').slick({
+        prevArrow: '<button class="slick-arrow slick-prev"><img src="images/slider/arrow-left.png" alt=""></button>',
+        nextArrow: '<button class="slick-arrow slick-next"><img src="images/slider/arrow-right.png" alt=""></button>',
+
+    });
 
 
 });
