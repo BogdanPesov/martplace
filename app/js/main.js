7f19628c121bdc22c8b7e37b724d417d83c8d4c1
BogdanPesov <bodyapesov@gmail.com>
2020-03-24 11:46:57 +0400

    category page done

--- app/js/main.js
+++ app/js/main.js
@@ -11,5 +11,39 @@ $(function() {
 
     });
 
-    var mixer = mixitup('.release__inner-box');
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        hide_min_max: true,
+        force_edges: true,
+        min: 0,
+        max: 320,
+        from_min: 30,
+        to_max: 300,
+        prefix: "$",
+
+    });
+
+
+    $(".category__page-item-star").rateYo({
+        rating: 4.5,
+        starWidth: "17px",
+        spacing: "3px",
+        readOnly: true
+    });
+
+    $('.lnr-list').on('click', function() {
+        $('.category__page-item').addClass('list');
+        $('.lnr-list').addClass('active');
+        $('.lnr-sort-amount-asc').removeClass('active');
+    });
+
+    $('.lnr-sort-amount-asc').on('click', function() {
+        $('.category__page-item').removeClass('list');
+        $('.lnr-sort-amount-asc').addClass('active');
+        $('.lnr-list').removeClass('active');
+    });
+
+
+    var mixer = mixitup('.release__inner-box, .category__page-box');
+
 });
